e7ef99ae2445b1dbe304a7a46f866f84680a76ac
carte7000 <simrob7000@gmail.com>
2015-01-22 15:17:29 -0500

    Première ébauche de la classe Demande et FileDemande.

--- ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/FileDemande.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/FileDemande.java
@@ -1,5 +1,26 @@
 package org.ClAssignateur.domain;
 
+import java.util.LinkedList;
+
+import java.util.Queue;
+import java.util.Date;
+
 public class FileDemande {
+
+	private Queue<Demande> _file = new LinkedList<Demande>();
+
+	public int Taille() {
+		return this._file.size();
+	}
+
+	public void Ajouter(Date dateDebut, Date dateFin, int nombreParticipant) {
+		Demande demandeAjoute = new Demande(dateDebut, dateFin, nombreParticipant);
+		
+		this._file.offer(demandeAjoute);
+	}
+
+	public void Vider() {
+		this._file.clear();
+	}
 
 }
--- ClAssignateur-Projet9/ClAssignateur-domain/src/test/java/org/ClAssignateur/domain/FileDemandeTest.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/test/java/org/ClAssignateur/domain/FileDemandeTest.java
@@ -1,14 +1,50 @@
 package org.ClAssignateur.domain;
 
 import static org.junit.Assert.*;
-
+import java.util.Date;
+import org.junit.Before;
 import org.junit.Test;
 import org.ClAssignateur.domain.FileDemande;
 
 public class FileDemandeTest {
 
+	private static final Date DATE_DEBUT = new Date(2015,07,1, 12,29,0);
+	private static final Date DATE_FIN = new Date(2015,07,1, 12,30,0);
+	private static final int NOMBRE_PARTICIPANT = 10;
+	
+	private FileDemande fileDemande;
+
+	@Before
+	public void creerFileDemande(){
+		fileDemande = new FileDemande();
+	}
+	
 	@Test
-	public void test() {
+	public void fileDemandeEstInitialementVide(){
+		boolean fileEstVide = fileDemande.Taille() == 0;
+		
+		assertTrue(fileEstVide);
+	}
+	
+	@Test
+	public void lorsqueOnAjouteUneDemandeAFileDemandeSaTailleAugmenteDeUn(){
+		int tailleInitiale = fileDemande.Taille();
+		fileDemande.Ajouter(DATE_DEBUT, DATE_FIN, NOMBRE_PARTICIPANT);
+		
+		int tailleDeLaFile = fileDemande.Taille();
+		int tailleDesiree = tailleInitiale+1;
+		
+		assertEquals(tailleDesiree,  tailleDeLaFile);
+	}
+	
+	@Test
+	public void lorsqueOnVideLaFileElleDevientVide(){
+		fileDemande.Ajouter(DATE_DEBUT, DATE_FIN, NOMBRE_PARTICIPANT);
+		
+		fileDemande.Vider();
+		boolean fileEstVide = fileDemande.Taille() == 0;
+		
+		assertTrue(fileEstVide);
 	}
 
 }
