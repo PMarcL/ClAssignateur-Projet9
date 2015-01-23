8dba14a599349475c972f34e0b0cfd18844657c7
Michaël Blanchet <mblanchet528@gmail.com>
2015-01-22 21:37:15 -0500

    Début du travail sur la classe Salle. Le constructeur et les méthodes GetCapacite et GetLocal sont terminés.

--- ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/Salle.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/Salle.java
@@ -2,4 +2,19 @@ package org.ClAssignateur.domain;
 
 public class Salle {
 
+	private String local;
+	private int capacite;
+
+	public Salle(String local, int capacite) {
+		this.local = local;
+		this.capacite = capacite;
+	}
+
+	public String GetLocal() {
+		return this.local;
+	}
+
+	public int GetCapacite() {
+		return this.capacite;
+	}
 }
