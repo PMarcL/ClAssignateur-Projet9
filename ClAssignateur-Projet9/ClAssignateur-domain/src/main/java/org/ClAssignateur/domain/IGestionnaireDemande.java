d2fcdc1a171fc9d6f119c2b2bf4e1befd88ec117
Michaël Blanchet <mblanchet528@gmail.com>
2015-01-22 21:13:14 -0500

    Ajout des méthodes dans l'interface IGestionnaireDemande

--- ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/IGestionnaireDemande.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/IGestionnaireDemande.java
@@ -1,5 +1,13 @@
 package org.ClAssignateur.domain;
 
+import java.util.Date;
+
 public interface IGestionnaireDemande {
 
+	public void SetFrequence(int frequence);
+
+	public void SetLimite(int limite);
+
+	public void AjouterDemande(Date debut, Date fin, int nbParticipants,
+			String organisateur);
 }
