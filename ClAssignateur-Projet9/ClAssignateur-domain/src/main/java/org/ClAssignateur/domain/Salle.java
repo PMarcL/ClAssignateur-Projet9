f1549fc9ae8e4bca1c4432bb6bd71d764b077eb6
Pierre-Marc Levasseur <pmleva@hotmail.com>
2015-01-23 19:22:59 -0500

    Test et code pour les getter de la classe Salle, ajout de test et de méthode à coder pour les réservation

--- ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/Salle.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/Salle.java
@@ -1,5 +1,38 @@
 package org.ClAssignateur.domain;
 
+import java.util.ArrayList;
+
 public class Salle {
+
+	private String nom;
+	private int capacite;
+	private ArrayList<Demande> reservations;
+
+	public Salle(String nomRecu, int capaciteRecu) {
+		nom = nomRecu;
+		capacite = capaciteRecu;
+		reservations = new ArrayList<Demande>();
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getCapacite() {
+		return capacite;
+	}
+
+	public boolean estDisponible(Demande demandeAVerifier) {
+		// TODO
+		return true;
+	}
+
+	public void placerReservation(Demande nouvelleReservation) {
+		reservations.add(nouvelleReservation);
+	}
+
+	public void enleverReservation(Demande reservationAEnlever) {
+		// TODO
+	}
 
 }
--- ClAssignateur-Projet9/ClAssignateur-domain/src/test/java/org/ClAssignateur/domain/SalleTest.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/test/java/org/ClAssignateur/domain/SalleTest.java
@@ -2,13 +2,49 @@ package org.ClAssignateur.domain;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
 import org.junit.Test;
 import org.ClAssignateur.domain.Salle;
 
 public class SalleTest {
 
+	private Salle salle;
+	private final String NOM_TEST = "nomSalle";
+	private final int CAPACITE_TEST = 100;
+
+	@Before
+	public void initialisation() {
+		salle = new Salle(NOM_TEST, CAPACITE_TEST);
+	}
+
 	@Test
-	public void test() {
+	public void UneNouvelleSalleRetourneSonNom() {
+		String nomRecu = salle.getNom();
+
+		assertEquals(NOM_TEST, nomRecu);
+	}
+
+	@Test
+	public void UneNouvelleSalleRetourneSaCapacite() {
+		int capaciteRecu = salle.getCapacite();
+
+		assertEquals(CAPACITE_TEST, capaciteRecu);
+	}
+
+	@Test
+	public void UneNouvelleSalleSansReservationEstDisponible() {
+		// assertTrue(salle.estDisponible(demandeAVerifier));
+	}
+
+	@Test
+	public void UneNouvelleSalleApresReservationEstPlusDisponibleALaDateDemande() {
+		// TODO
+	}
+
+	@Test
+	public void SalleAvecUneReservationApresRetraitDeLaReservationEstDisponible() {
+		// TODO
 	}
 
 }
