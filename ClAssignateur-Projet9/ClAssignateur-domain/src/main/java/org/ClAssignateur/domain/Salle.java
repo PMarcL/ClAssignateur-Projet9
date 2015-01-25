232d2262cf69bea6767370c9a407692de7139992
Michaël Blanchet <mblanchet528@gmail.com>
2015-01-25 14:40:57 -0500

    Revert "Début du travail sur la classe Salle."
    This reverts commit 8dba14a599349475c972f34e0b0cfd18844657c7.

--- ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/Salle.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/Salle.java
@@ -2,19 +2,4 @@ package org.ClAssignateur.domain;
 
 public class Salle {
 
-	private String local;
-	private int capacite;
-
-	public Salle(String local, int capacite) {
-		this.local = local;
-		this.capacite = capacite;
-	}
-
-	public String GetLocal() {
-		return this.local;
-	}
-
-	public int GetCapacite() {
-		return this.capacite;
-	}
 }
--- ClAssignateur-Projet9/ClAssignateur-domain/src/test/java/org/ClAssignateur/domain/SalleTest.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/test/java/org/ClAssignateur/domain/SalleTest.java
@@ -2,29 +2,13 @@ package org.ClAssignateur.domain;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.ClAssignateur.domain.Salle;
 
 public class SalleTest {
 
-	private final int UNE_CAPACITE_QUELCONQUE = 12;
-	private final String UN_LOCAL_QUELCONQUE = "PLT-2551";
-
-	private Salle salleTestee;
-
-	@Before
-	public void initialement() {
-		salleTestee = new Salle(UN_LOCAL_QUELCONQUE, UNE_CAPACITE_QUELCONQUE);
-	}
-
 	@Test
-	public void uneNouvelleSalleConnaitSonLocal() {
-		assertEquals(UN_LOCAL_QUELCONQUE, salleTestee.GetLocal());
-	}
-
-	@Test
-	public void uneNouvelleSalleConnaitSaCapacite() {
-		assertEquals(UNE_CAPACITE_QUELCONQUE, salleTestee.GetCapacite());
+	public void test() {
 	}
 
 }
