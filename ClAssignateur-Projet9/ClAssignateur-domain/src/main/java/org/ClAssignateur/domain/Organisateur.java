bd011cb44c9cd65787270fc2e7eaa3abc20e789e
Michaël Blanchet <mblanchet528@gmail.com>
2015-01-25 15:58:57 -0500

    Ajout de la gestion des noms vides dans le constructeur de la classe Organisateur

--- ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/Organisateur.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/Organisateur.java
@@ -5,6 +5,10 @@ public class Organisateur {
 	private String nom;
 
 	public Organisateur(String nom) {
+		if (nom == "")
+			throw new IllegalArgumentException(
+					"Le nom de l'organisateur ne peut pas être vide.");
+
 		this.nom = nom;
 	}
 
--- ClAssignateur-Projet9/ClAssignateur-domain/src/test/java/org/ClAssignateur/domain/OrganisateurTest.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/test/java/org/ClAssignateur/domain/OrganisateurTest.java
@@ -7,11 +7,17 @@ import org.junit.Test;
 public class OrganisateurTest {
 
 	private final String UN_NOM_QUELCONQUE = "John Doe";
+	private final String UN_NOM_VIDE = "";
 
 	@Test
 	public void unNouvelOrganisateurQuandCreeConnaitSonNom() {
 		Organisateur organisateurATester = new Organisateur(UN_NOM_QUELCONQUE);
 		assertEquals(UN_NOM_QUELCONQUE, organisateurATester.GetNom());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void unNouvelOrganisateurQuandCreeNePeutAvoirUnNomVide() {
+		Organisateur organisateurATester = new Organisateur(UN_NOM_VIDE);
 	}
 
 }
