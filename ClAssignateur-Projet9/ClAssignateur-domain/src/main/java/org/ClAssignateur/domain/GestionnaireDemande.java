b7304c69b19c860b6ee91e7ac81ec841a3433e83
Pierre-Marc Levasseur <pmleva@hotmail.com>
2015-02-02 09:14:31 -0500

    retrait des méthodes pas implémentées dans GestionnaireDemande.

--- ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/GestionnaireDemande.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/GestionnaireDemande.java
@@ -2,13 +2,4 @@ package org.ClAssignateur.domain;
 
 public class GestionnaireDemande {
 
-	public int getFrequence() {
-		return 0;
-	}
-
-	public void verifierConditionAssignation() {
-		
-		
-	}
-
 }
