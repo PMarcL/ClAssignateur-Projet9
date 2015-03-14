66e4cb64b4d1943b50d1d8f3f8caa6fcfc712763
pascal129 <pascal.labb129@gmail.com>
2015-03-13 23:20:27 -0400

    Modification de la Notification par Courriel

--- ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/NotificateurCourriel.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/NotificateurCourriel.java
@@ -1,12 +1,23 @@
 package org.ClAssignateur.domain;
 
-import java.net.PasswordAuthentication;
 import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
 
 public class NotificateurCourriel implements Notificateur {
 	
+	
+	
+	private static final String UTILISATEUR_SMTP = "";
+	private static final String MOT_DE_PASSE_SMTP = "";
+	private static final String EXPEDITEUR_SMTP = "pascal.labb129@gmail.com";
 	private static final String SERVEUR_SMTP = "smtp.gmail.com";
-
     
 	public void notifier(MessageNotification message, Employe destinataire)  {
 		try{
@@ -19,7 +30,6 @@ public class NotificateurCourriel implements Notificateur {
 			Transport.send(messageMime);	
 			
 		 }catch (MessagingException messagingException) {
-			 System.out.println("MessagingException");
 			 messagingException.printStackTrace();
 	      }
 	}
