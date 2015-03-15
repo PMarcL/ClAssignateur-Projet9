e88cdfd2ac5fbcf1b44bce1ed412c49a6a165612
carte7000 <simrob7000@gmail.com>
2015-03-14 21:02:11 -0400

    Modification Notificateur

--- ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/Notificateur.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/Notificateur.java
@@ -9,18 +9,27 @@ public class Notificateur {
 	}
 
 	public void notifierAnnulation(Demande demandeAAnnuler) {
+		String contenu = "La demande nommée:" + demandeAAnnuler.getTitre() + " à été annulée.";
 
+		notifierDirigeants(contenu, demandeAAnnuler);
+
+		for (Employe participant : demandeAAnnuler.getParticipants()) {
+			this.notifictionStrategie.notifier(contenu, participant);
+		}
 	}
 
 	public void notifierSucces(Demande demandeAvecSalleDisponible, Salle salleDisponible) {
-		String contenu = "La salle: " + salleDisponible.getNom() + " a été réservée avec succès";
-
-		this.notifictionStrategie.notifier(contenu, demandeAvecSalleDisponible.getOrganisateur());
-		this.notifictionStrategie.notifier(contenu, demandeAvecSalleDisponible.getResponsable());
+		String contenu = "La salle: " + salleDisponible.getNom() + " a été réservée avec succès.";
+		notifierDirigeants(contenu, demandeAvecSalleDisponible);
 	}
 
 	public void notifierEchec(Demande demandeNePouvantPasEtreAssignee) {
-		this.notifictionStrategie.notifier("", demandeNePouvantPasEtreAssignee.getOrganisateur());
-		this.notifictionStrategie.notifier("", demandeNePouvantPasEtreAssignee.getResponsable());
+		String contenu = "Aucune salle n'a pu être assignée avec votre demande.";
+		notifierDirigeants(contenu, demandeNePouvantPasEtreAssignee);
+	}
+
+	private void notifierDirigeants(String contenu, Demande demande) {
+		this.notifictionStrategie.notifier(contenu, demande.getOrganisateur());
+		this.notifictionStrategie.notifier(contenu, demande.getResponsable());
 	}
 }
--- ClAssignateur-Projet9/ClAssignateur-domain/src/test/java/org/ClAssignateur/domain/NotificateurTest.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/test/java/org/ClAssignateur/domain/NotificateurTest.java
@@ -1,11 +1,14 @@
 package org.ClAssignateur.domain;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.BDDMockito.*;
 
 public class NotificateurTest {
 
+	private static final String TITRE_DEMANDE = "RENCONTRE_COURS_QUALITE";
 	private String NOM_DE_SALLE = "A100";
 	private Salle SALLE = new Salle(100, NOM_DE_SALLE);
 
@@ -22,6 +25,7 @@ public class NotificateurTest {
 		notificateur = new Notificateur(notificationStrategie);
 		organisateur = mock(Employe.class);
 		demande = mock(Demande.class);
+		given(demande.getTitre()).willReturn(TITRE_DEMANDE);
 		given(demande.getOrganisateur()).willReturn(organisateur);
 		responsable = mock(Employe.class);
 		given(demande.getResponsable()).willReturn(responsable);
@@ -41,7 +45,7 @@ public class NotificateurTest {
 
 	@Test
 	public void quandNotifierSuccesDevraitAppelerNotifierAvecBonMessage() {
-		String MESSAGE_DESIRE = "La salle: " + NOM_DE_SALLE + " a été réservée avec succès";
+		String MESSAGE_DESIRE = "La salle: " + NOM_DE_SALLE + " a été réservée avec succès.";
 
 		notificateur.notifierSucces(demande, SALLE);
 
@@ -62,8 +66,46 @@ public class NotificateurTest {
 
 	@Test
 	public void quandNotifierEchecDevraitAppelerNotifierAvecBonMessage() {
-		String MESSAGE_DESIRE = "La salle: " + NOM_DE_SALLE + " a été réservée avec succès";
+		String MESSAGE_DESIRE = "Aucune salle n'a pu être assignée avec votre demande.";
 
 		notificateur.notifierEchec(demande);
+
+		verify(notificationStrategie, times(2)).notifier(eq(MESSAGE_DESIRE), any(Employe.class));
+	}
+
+	@Test
+	public void quandNotifierAnnulationDevraitAppelerNotifierAvecOrganisateur() {
+		notificateur.notifierAnnulation(demande);
+		verify(notificationStrategie).notifier(anyString(), eq(organisateur));
+	}
+
+	@Test
+	public void quandNotifierAnnulationDevraitAppelerNotifierAvecResponsable() {
+		notificateur.notifierAnnulation(demande);
+		verify(notificationStrategie).notifier(anyString(), eq(responsable));
+	}
+
+	@Test
+	public void quandNotifierAnnulationDevraitAppelerNotifierAvecTousLesParticipant() {
+		Employe participant1 = mock(Employe.class);
+		Employe participant2 = mock(Employe.class);
+		ArrayList<Employe> participants = new ArrayList<Employe>();
+		participants.add(participant1);
+		participants.add(participant2);
+		given(demande.getParticipants()).willReturn(participants);
+
+		notificateur.notifierAnnulation(demande);
+
+		verify(notificationStrategie).notifier(anyString(), eq(participant1));
+		verify(notificationStrategie).notifier(anyString(), eq(participant2));
+	}
+
+	@Test
+	public void quandNotifierAnnulationDevraitAppelerNotifierAvecBonMessage() {
+		String MESSAGE_DESIRE = "La demande nommée:" + TITRE_DEMANDE + " à été annulée.";
+
+		notificateur.notifierAnnulation(demande);
+
+		verify(notificationStrategie, times(2)).notifier(eq(MESSAGE_DESIRE), any(Employe.class));
 	}
 }
