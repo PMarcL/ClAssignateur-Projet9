db5e1444683284d54aa285b2a8fcf75198efb057
Michaël Blanchet <mblanchet528@gmail.com>
2015-03-21 14:45:45 -0400

    Ajout de la classe NotificationStrategieCourrielSsl (gestion des exception doit être révisée pour respecter Liskov) Ajout de l'interface ConfigurationServeurSmtp

---
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/main/java/org/ClAssignateur/domain/ConfigurationServeurSmtp.java
@@ -0,0 +1,11 @@
+package org.ClAssignateur.domain;
+
+public interface ConfigurationServeurSmtp {
+
+	public String getAdresseServeurSmtp();
+
+	public String getNomUtilisateur();
+
+	public String getMotDePasse();
+
+}
