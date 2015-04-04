a78130a100fa2724f17cb7ee3de954cf309b72ef
Michaël Blanchet <mblanchet528@gmail.com>
2015-04-04 12:15:01 -0400

    Configuration de JBehave pour qu'il fonctionne en français et traduction des 2 stories existantes

--- ClAssignateur-Projet9/ClAssignateur-TestsAcceptationUtilisateur/src/main/java/org/ClAssignateur/testsAcceptationUtilisateur/RecitsClAssignateur.java
+++ ClAssignateur-Projet9/ClAssignateur-TestsAcceptationUtilisateur/src/main/java/org/ClAssignateur/testsAcceptationUtilisateur/RecitsClAssignateur.java
@@ -6,25 +6,32 @@ import static org.jbehave.core.reporters.Format.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
+import org.ClAssignateur.testsAcceptationUtilisateur.etapes.AssignerEnLotSallesDemandesEtapes;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.embedder.Embedder;
 import org.jbehave.core.failures.FailingUponPendingStep;
+import org.jbehave.core.i18n.LocalizedKeywords;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.StoryFinder;
 import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.parsers.RegexStoryParser;
+import org.jbehave.core.reporters.CrossReference;
 import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
-import org.jbehave.core.steps.ScanningStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
 
 public class RecitsClAssignateur extends JUnitStories {
 
 	private Format[] formats = new Format[] { CONSOLE };
 	private StoryReporterBuilder rapporteurBuilder = new StoryReporterBuilder()
+			.withKeywords(new LocalizedKeywords(Locale.FRENCH))
 			.withCodeLocation(codeLocationFromClass(RecitsClAssignateur.class)).withFailureTrace(true)
-			.withFailureTraceCompression(true).withDefaultFormats().withFormats(formats);
+			.withFailureTraceCompression(true).withDefaultFormats().withFormats(formats)
+			.withCrossReference(new CrossReference());
 	private Embedder embedder = new Embedder();
 
 	public static void main(String[] args) {
@@ -38,20 +45,22 @@ public class RecitsClAssignateur extends JUnitStories {
 
 	@Override
 	public Configuration configuration() {
-		return new MostUsefulConfiguration().usePendingStepStrategy(new FailingUponPendingStep())
+		LocalizedKeywords motCles = new LocalizedKeywords(new Locale("fr"));
+		return new MostUsefulConfiguration().useKeywords(motCles).usePendingStepStrategy(new FailingUponPendingStep())
+				.useStoryParser(new RegexStoryParser(motCles))
 				.useStoryLoader(new LoadFromClasspath(getClass().getClassLoader()))
 				.useStoryReporterBuilder(rapporteurBuilder);
 	}
 
 	@Override
 	public InjectableStepsFactory stepsFactory() {
-		return new ScanningStepsFactory(configuration(), getClass());
+		return new InstanceStepsFactory(configuration(), new AssignerEnLotSallesDemandesEtapes());
 	}
 
 	@Override
 	protected List<String> storyPaths() {
 		return new StoryFinder().findPaths(codeLocationFromClass(this.getClass()).getFile(),
-				asList("**/*.story", "*.story"), null);
+				asList("**/*.histoire", "*.histoire"), null);
 	}
 
 }
--- ClAssignateur-Projet9/ClAssignateur-TestsAcceptationUtilisateur/src/main/resources/stories/maximiserLesPlacesDansSalle.story
+++
@@ -1,6 +0,0 @@
-Maximiser les places dans la salle
-
-Narrative:
-In order to maximiser l'utilisation des places dans les salles de réunion
-As a entreprise
-I want to utiliser la salle la plus petite qui répond aux besoins de la réunion
