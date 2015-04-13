557b53275a63545650a73cc4d6bfd4f32a749ed8
pacos15 <pacotesarrazin@gmail.com>
2015-04-12 20:32:53 -0400

    ajout du cas de test ou il y a un invalidCourrielExeption. donne bad request.

--- ClAssignateur-Projet9/ClAssignateur-domain/src/test/java/org/ClAssignateur/interfaces/AjoutDemandeRessourceTest.java
+++ ClAssignateur-Projet9/ClAssignateur-domain/src/test/java/org/ClAssignateur/interfaces/AjoutDemandeRessourceTest.java
@@ -1,7 +1,8 @@
 package org.ClAssignateur.interfaces;
 
-import java.net.URI;
+import org.ClAssignateur.domain.groupe.AdresseCourrielInvalideException;
 
+import java.net.URI;
 import java.util.UUID;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.Response;
@@ -16,37 +17,46 @@ public class AjoutDemandeRessourceTest {
 
 	final private UUID DEMANDE_ID = UUID.randomUUID();
 	private final String COURRIEL_ORGANISATEUR = "courrielOrganisateur@courriel.com";
+	private final AdresseCourrielInvalideException COURRIEL_INVALIDE_EXCEPTION = new AdresseCourrielInvalideException("ERREUR_COURRIEL");
 	private final String EMPLACEMENT_DEMANDE_AJOUTE = "/demandes/" + COURRIEL_ORGANISATEUR + "/" + DEMANDE_ID.toString();
 	
-	private ReservationDemandeDTO demandeDTOValide;
+	private ReservationDemandeDTO demandeDTO;
 	private ServiceDemande service;
 	private ReservationDemandeDTOAssembleur reservationDemandeAssembleur;
-	private Demande demandeValide;
+	private Demande demande;
 	
 	private AjoutDemandeRessource ressource;
 	
 	@Before
 	public void initialement() {
+		demandeDTO = mock(ReservationDemandeDTO.class);
+		demandeDTO.courrielOrganisateur = COURRIEL_ORGANISATEUR;
 		service = mock(ServiceDemande.class);
 		reservationDemandeAssembleur = mock(ReservationDemandeDTOAssembleur.class);
-		demandeValide = mock(Demande.class);
-		demandeDTOValide = new ReservationDemandeDTO();
-		demandeDTOValide.courrielOrganisateur = COURRIEL_ORGANISATEUR;
-		given(demandeValide.getID()).willReturn(DEMANDE_ID);
-		given(reservationDemandeAssembleur.assemblerDemande(demandeDTOValide)).willReturn(demandeValide);
+		demande = mock(Demande.class);
+		given(demande.getID()).willReturn(DEMANDE_ID);
 		
 		ressource = new AjoutDemandeRessource(service, reservationDemandeAssembleur);
 	}
 	
 	@Test
 	public void lorsqueAjouterUneDemandeValideDevraitRetrouverStatusCREATED(){
-		Response reponse = ressource.ajouterDemande(demandeDTOValide);
+		given(reservationDemandeAssembleur.assemblerDemande(demandeDTO)).willReturn(demande);
+		Response reponse = ressource.ajouterDemande(demandeDTO);
 		assertEquals(Status.CREATED.getStatusCode(), reponse.getStatus());
 	}
 	
 	@Test
 	public void lorsqueAjouterUneDemandeValideDevraitRetrouverLeBonEmplacementDeLaDemandeAjoute(){
-		Response reponse = ressource.ajouterDemande(demandeDTOValide);
+		given(reservationDemandeAssembleur.assemblerDemande(demandeDTO)).willReturn(demande);
+		Response reponse = ressource.ajouterDemande(demandeDTO);
 		assertEquals(EMPLACEMENT_DEMANDE_AJOUTE, reponse.getLocation().toString());
+	}
+	
+	@Test
+	public void lorsqueAjouterUneDemandeAvecCourrielInvalideDevraitRetrouverStatusBAD_REQUEST(){
+		given(reservationDemandeAssembleur.assemblerDemande(demandeDTO)).willThrow(COURRIEL_INVALIDE_EXCEPTION);
+		Response reponse = ressource.ajouterDemande(demandeDTO);
+		assertEquals(Status.BAD_REQUEST.getStatusCode(), reponse.getStatus());
 	}
 }
