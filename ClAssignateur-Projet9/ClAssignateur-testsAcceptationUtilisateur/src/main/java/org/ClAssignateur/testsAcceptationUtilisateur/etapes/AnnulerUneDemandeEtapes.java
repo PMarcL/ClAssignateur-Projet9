24dfec359004a16da83906ac75d75e14f65958d0
Pierre-Marc Levasseur <pmleva@hotmail.com>
2015-04-17 15:46:32 -0400

    Ajout des scénario pour la story 6 et ajout des tests (sans l'implémentation)

---
+++ ClAssignateur-Projet9/ClAssignateur-testsAcceptationUtilisateur/src/main/java/org/ClAssignateur/testsAcceptationUtilisateur/etapes/AnnulerUneDemandeEtapes.java
@@ -0,0 +1,59 @@
+package org.ClAssignateur.testsAcceptationUtilisateur.etapes;
+
+import org.jbehave.core.annotations.BeforeScenario;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+public class AnnulerUneDemandeEtapes {
+
+	@BeforeScenario
+	public void initialisationScenario() {
+
+	}
+
+	@Given("une demande assignée")
+	public void givenUneDemandeAssignée() {
+
+	}
+
+	@Given("une demande en attente de traitement")
+	public void givenUneDemandeEnAttenteDeTraitement() {
+
+	}
+
+	@When("on annule la demande assignée")
+	public void whenOnAnnuleLaDemandeAssignee() {
+
+	}
+
+	@When("on annule la demande en attente")
+	public void whenOnAnnuleLaDemandeEnAttente() {
+
+	}
+
+	@Then("la demande assignée est annulée")
+	public void thenLaDemandeEstAnnulee() {
+
+	}
+
+	@Then("la demande en attente est annulée")
+	public void thenLaDemandeEnAttenteEstAnnulée() {
+
+	}
+
+	@Then("la demande est archivée")
+	public void thenLaDemandeEstArchivee() {
+
+	}
+
+	@Then("la salle assignée à cette demande lui est retirée")
+	public void thenLaSalleAssigneeACetteDemandeLuiEstRetiree() {
+
+	}
+
+	@Then("le satut de la demande est changer pour un statut d'annulation")
+	public void thenLeStatutDeLaDemandeEstChanger() {
+
+	}
+}
