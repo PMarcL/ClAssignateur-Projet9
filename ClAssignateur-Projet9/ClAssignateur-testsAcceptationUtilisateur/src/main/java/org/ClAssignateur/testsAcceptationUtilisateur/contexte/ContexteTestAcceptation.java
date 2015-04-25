fe7b9aa670df249b9c9cec995278c3eb103621f5
Michaël Blanchet <mblanchet528@gmail.com>
2015-04-25 15:54:52 -0400

    Ajout contexte tests acceptation

---
+++ ClAssignateur-Projet9/ClAssignateur-testsAcceptationUtilisateur/src/main/java/org/ClAssignateur/testsAcceptationUtilisateur/contexte/ContexteTestAcceptation.java
@@ -0,0 +1,55 @@
+package org.ClAssignateur.testsAcceptationUtilisateur.contexte;
+
+import org.ClAssignateur.contexte.Contexte;
+import org.ClAssignateur.domaine.assignateur.AssignateurSalle;
+import org.ClAssignateur.domaine.assignateur.strategies.SelectionSalleOptimaleStrategie;
+import org.ClAssignateur.domaine.demandes.ConteneurDemandes;
+import org.ClAssignateur.domaine.notification.Notificateur;
+import org.ClAssignateur.domaine.salles.SallesEntrepot;
+import org.ClAssignateur.persistance.EnMemoireDemandeEntrepot;
+import org.ClAssignateur.persistance.EnMemoireSallesEntrepot;
+import org.ClAssignateur.services.localisateur.LocalisateurServices;
+import org.ClAssignateur.services.reservations.DeclencheurAssignateurSalle;
+import org.ClAssignateur.testsAcceptationUtilisateur.fakes.MinuterieFake;
+import org.ClAssignateur.testsAcceptationUtilisateur.fakes.NotificationStrategieSilencieuse;
+
+public class ContexteTestAcceptation extends Contexte {
+
+	private SallesEntrepot sallesEntrepot;
+	private ConteneurDemandes conteneurDemandes;
+
+	public ContexteTestAcceptation() {
+		initialisation();
+	}
+
+	private void initialisation() {
+		this.sallesEntrepot = new EnMemoireSallesEntrepot();
+		this.conteneurDemandes = new ConteneurDemandes(new EnMemoireDemandeEntrepot(), new EnMemoireDemandeEntrepot());
+	}
+
+	@Override
+	protected void enregistrerServices() {
+		LocalisateurServices.getInstance().enregistrer(SallesEntrepot.class, this.sallesEntrepot);
+		LocalisateurServices.getInstance().enregistrer(ConteneurDemandes.class, this.conteneurDemandes);
+		LocalisateurServices.getInstance().enregistrer(DeclencheurAssignateurSalle.class,
+				creerDeclencheurAssignateurSalle());
+	}
+
+	private DeclencheurAssignateurSalle creerDeclencheurAssignateurSalle() {
+		Notificateur notificateur = new Notificateur(new NotificationStrategieSilencieuse());
+
+		AssignateurSalle assignateur = new AssignateurSalle(this.sallesEntrepot, new SelectionSalleOptimaleStrategie());
+		return new DeclencheurAssignateurSalle(new MinuterieFake(), this.conteneurDemandes, assignateur, notificateur);
+	}
+
+	@Override
+	protected void injecterDonnees() {
+	}
+
+	public void reinitialiser() {
+		initialisation();
+		LocalisateurServices.getInstance().remettreAZero();
+		appliquer();
+	}
+
+}
