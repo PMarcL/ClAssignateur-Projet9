36544af677ea126f62b78fb589f159c63702b8de
Michaël Blanchet <mblanchet528@gmail.com>
2015-04-26 18:44:15 -0400

    Retravaillé sur récit AnnulerUneDemande pour fonctionner avec nouvel Assignateur

--- ClAssignateur-Projet9/ClAssignateur-testsAcceptationUtilisateur/src/main/java/org/ClAssignateur/testsAcceptationUtilisateur/fakes/ConteneurDemandesFake.java
+++ ClAssignateur-Projet9/ClAssignateur-testsAcceptationUtilisateur/src/main/java/org/ClAssignateur/testsAcceptationUtilisateur/fakes/ConteneurDemandesFake.java
@@ -9,20 +9,24 @@ import org.ClAssignateur.domaine.demandes.DemandesEntrepot;
 
 public class ConteneurDemandesFake extends ConteneurDemandes {
 
-	private Queue<Demande> demandesTraitees;
+	private Queue<Demande> demandesArchivees;
 
 	public ConteneurDemandesFake(DemandesEntrepot demandesEnAttente, DemandesEntrepot demandesArchivees) {
 		super(demandesEnAttente, demandesArchivees);
-		this.demandesTraitees = new LinkedList<>();
+		this.demandesArchivees = new LinkedList<>();
 	}
 
 	@Override
 	public void archiverDemande(Demande demande) {
 		super.archiverDemande(demande);
-		this.demandesTraitees.add(demande);
+		this.demandesArchivees.add(demande);
 	}
 
 	public Demande retirerDemandeTraitee() {
-		return this.demandesTraitees.poll();
+		return this.demandesArchivees.poll();
+	}
+
+	public boolean demandesEstArchivee(Demande demande) {
+		return this.demandesArchivees.contains(demande);
 	}
 }
---
+++ ClAssignateur-Projet9/ClAssignateur-testsAcceptationUtilisateur/src/main/java/org/ClAssignateur/testsAcceptationUtilisateur/fixtures/DemandeConstructeur.java
@@ -0,0 +1,49 @@
+package org.ClAssignateur.testsAcceptationUtilisateur.fixtures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ClAssignateur.domaine.contacts.ContactsReunion;
+import org.ClAssignateur.domaine.contacts.InformationsContact;
+import org.ClAssignateur.domaine.demandes.Demande;
+import org.ClAssignateur.domaine.demandes.priorite.Priorite;
+
+public class DemandeConstructeur {
+
+	private final int NOMBRE_PARTICIPANTS_PAR_DEFAUT = 25;
+	private final InformationsContact ORGANISATEUR = new InformationsContact("organisateur@domaine.com");
+	private final InformationsContact RESPONSABLE = new InformationsContact("responsable@domaine.com");
+	private final String TITRE_PAR_DEFAUT = "Nouvelle réunion";
+	private final Priorite PRIORITE_PAR_DEFAUT = Priorite.tresHaute();
+
+	private int nbParticipants;
+	private InformationsContact organisateur;
+	private InformationsContact responsable;
+	private List<InformationsContact> participants;
+	private String titre;
+	private Priorite priorite;
+
+	public DemandeConstructeur() {
+		this.nbParticipants = NOMBRE_PARTICIPANTS_PAR_DEFAUT;
+		this.organisateur = ORGANISATEUR;
+		this.responsable = RESPONSABLE;
+		this.participants = new ArrayList<>();
+		this.titre = TITRE_PAR_DEFAUT;
+		this.priorite = PRIORITE_PAR_DEFAUT;
+	}
+
+	public DemandeConstructeur titre(String titre) {
+		this.titre = titre;
+		return this;
+	}
+
+	public Demande construireDemande() {
+		ContactsReunion contacts = creerContactsReunion();
+		return new Demande(this.nbParticipants, contacts, this.titre, this.priorite);
+	}
+
+	private ContactsReunion creerContactsReunion() {
+		return new ContactsReunion(this.organisateur, this.responsable, this.participants);
+	}
+
+}
